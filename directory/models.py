402381c4e58208df8a40317e341360081c0cbfe2
Alvaro <alvarobolanos@gmail.com>
2018-03-08 15:03:58 -0500

    working app

--- directory/models.py
+++ directory/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Agent(models.Model):
+    name = models.CharField(max_length=40)
+    title = models.CharField(max_length=100)
+    hired_language = models.CharField(max_length=2)
+    email = models.CharField(max_length=100)
+    image = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
